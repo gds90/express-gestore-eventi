153fe8034631df9c27bebd8124eeba10f61a18bd
gds90 <you@example.com>
2024-05-31 14:46:12 +0200

    Bonus: create le rotte per le prenotazioni, con relativo controller

--- routers/eventsRouter.js
+++ routers/eventsRouter.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const eventsController = require("../controllers/eventsController.js");
+const reservationsController = require("../controllers/reservationsController.js");
 router.use(express.urlencoded({ extended: true }));
 
-// Rotte
+// Rotte eventi
 router.get("/", eventsController.index);
 router.post("/", eventsController.store);
 router.put("/:event", eventsController.update);
+
+// Rotte prenotazioni
+router.get("/:event/reservations", reservationsController.index);
+router.post("/:event/reservations", reservationsController.store);
+router.delete("/:event/reservations/:reservation", reservationsController.destroy);
 
 module.exports = router;
