a9fa467a7c79c84b9bffb149b28e47317efeca71
gds90 <you@example.com>
2024-05-31 14:51:02 +0200

    Bonus: creato model Reservation con le relative proprietà indicate

---
+++ model/Reservation.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+class Reservation {
+    constructor(id, firstName, lastName, email, eventId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.eventId = eventId;
+    }
+}
